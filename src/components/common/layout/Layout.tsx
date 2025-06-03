adb1e23f2a268e741cfdaf985031e00f1ed1cda5
lh7721004 <lh7721004@gmail.com>
2025-06-03 19:02:16 +0900

    feature: 공통 레이아웃 경로에 따른 하이라이팅 적용

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,14 +1,34 @@
 // src/components/Layout.tsx
 import { Outlet } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
 export default function Layout() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const getLinkClass = (path: string) => {
+        return location.pathname === path
+        ? "cursor-pointer font-bold underline"
+        : "cursor-pointer";
+    };
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-blue-600 text-white p-4">공통 헤더</header>
-      <main className="flex-1 p-4">
+      <header className="bg-[#F7FAFC] text-[#0D141C] p-4 flex flex-row justify-between border-b-2 border-[#D1DBE8]">
+        <div className="flex flex-row gap-1">
+            <div className="h-full flex flex-col justify-center">
+                <div className="w-4 h-4 bg-black"></div>
+            </div>
+            <div className="font-black">FileDrop</div>    
+        </div>
+        <div className="flex flex-row gap-4">
+            <div className={getLinkClass("/access")} onClick={()=>{navigate('/access')}}>Access</div>
+            <div className={getLinkClass("/share")} onClick={()=>{navigate('/share')}}>Share</div>
+            <div className={getLinkClass("/")} onClick={()=>{navigate('/')}}>Home</div>
+        </div>
+      </header>
+      <main className="flex-1 p-4 bg-[#F7FAFC]">
         <Outlet/>
       </main>
-      <footer className="bg-gray-800 text-white p-4">공통 푸터</footer>
+      {/* <footer className="bg-gray-800 text-white p-4">공통 푸터</footer> */}
     </div>
   );
 }
