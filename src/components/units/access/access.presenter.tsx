3e93023949ac6f335c83926e23b587afb78cb06a
lh7721004 <lh7721004@gmail.com>
2025-06-03 19:03:28 +0900

    feature: 파일 접근 페이지 UI 작업 완료

--- src/components/units/access/access.presenter.tsx
+++ src/components/units/access/access.presenter.tsx
@@ -1,3 +1,15 @@
 export default function AccessUI() {
-    return (<></>)
+    return (
+    <div className="flex flex-row justify-center">
+        <div className="flex flex-col justify-center h-full w-full text-center px-16 gap-3 py-8">
+            <div className="text-3xl font-bold">Access Your File</div>
+            <div>Enter the authcode to access your files</div>
+            <div className="flex flex-row justify-center w-full py-2">
+                <input placeholder="Auth Code Here" className="bg-[#E8EDF2] rounded-xl p-4 w-full max-w-96"></input>
+            </div>
+            <div className="flex flex-row justify-center w-full">
+                <button className="rounded-xl p-4 w-full max-w-32 text-[#F7FAFC] font-bold bg-[#1A78E5]">Access Files</button>
+            </div>
+        </div>
+    </div>)
 }
